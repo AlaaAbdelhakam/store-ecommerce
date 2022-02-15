3e73655049aab1bdec5565795f7454c4d058687b
AlaaAbdelhakam <alaa.abdelhakam@hotmail.com>
2022-02-15 18:56:36 +0200

    =end shippings

--- app/Http/Controllers/Dashboard/SettingsController.php
+++ app/Http/Controllers/Dashboard/SettingsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Dashboard;
 use App\Models\Setting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\ShippingsRequest;
+use DB;
 
 class SettingsController extends Controller
 {
