1ec0a2806efc910006a432860e571b75f01ade04
AlaaAbdelhakam <alaa.abdelhakam@hotmail.com>
2022-02-17 12:14:32 +0200

    =end categories bransh

--- resources/views/dashboard/brands/index.blade.php
+++ resources/views/dashboard/brands/index.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.admin')
 @section('content')
 
@@ -9,7 +10,7 @@
                     <div class="row breadcrumbs-top">
                         <div class="breadcrumb-wrapper col-12">
                             <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">الرئيسية</a>
+                                <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">الرئيسية</a>
                                 </li>
                                 <li class="breadcrumb-item active"> الماركات التجارية
                                 </li>
@@ -25,8 +26,9 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="card-title">جميع الماركات التجارية </h4>
-                                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                    <h4 class="card-title">جميع الماركات التجارية   </h4>
+                                    <a class="heading-elements-toggle"><i
+                                            class="la la-ellipsis-v font-medium-3"></i></a>
                                     <div class="heading-elements">
                                         <ul class="list-inline mb-0">
                                             <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
@@ -42,41 +44,41 @@
 
                                 <div class="card-content collapse show">
                                     <div class="card-body card-dashboard">
-                                        <table class="table display nowrap table-striped table-bordered scroll-horizontal">
+                                        <table
+                                            class="table display nowrap table-striped table-bordered scroll-horizontal">
                                             <thead class="">
-                                                <tr>
-                                                    <th>الاسم </th>
-                                                    <th>الحالة</th>
-                                                    <th>صوره الماركة</th>
-                                                    <th>الإجراءات</th>
-                                                </tr>
+                                            <tr>
+                                                <th>الاسم </th>
+                                                <th>الحالة</th>
+                                                <th>صوره الماركة</th>
+                                                <th>الإجراءات</th>
+                                            </tr>
                                             </thead>
                                             <tbody>
 
-                                                @isset($brands)
-                                                    @foreach ($brands as $brand)
-                                                        <tr>
-                                                            <td>{{ $brand->name }}</td>
-                                                            <td>{{ $brand->getActive() }}</td>
-                                                            <td> <img style="width: 150px; height: 100px;"
-                                                                    src="{{ $brand->photo }}"></td>
-                                                            <td>
-                                                                <div class="btn-group" role="group"
-                                                                    aria-label="Basic example">
-                                                                    <a href="{{ route('admin.brands.edit', $brand->id) }}"
-                                                                        class="btn btn-outline-primary btn-min-width box-shadow-3 mr-1 mb-1">تعديل</a>
+                                            @isset($brands)
+                                                @foreach($brands as $brand)
+                                                    <tr>
+                                                        <td>{{$brand -> name}}</td>
+                                                         <td>{{$brand -> getActive()}}</td>
+                                                        <td> <img style="width: 150px; height: 100px;" src="{{$brand -> photo }}"></td>
+                                                        <td>
+                                                            <div class="btn-group" role="group"
+                                                                 aria-label="Basic example">
+                                                                <a href="{{route('admin.brands.edit',$brand -> id)}}"
+                                                                   class="btn btn-outline-primary btn-min-width box-shadow-3 mr-1 mb-1">تعديل</a>
 
 
-                                                                    <a href="{{ route('admin.brands.delete', $brand->id) }}"
-                                                                        class="btn btn-outline-danger btn-min-width box-shadow-3 mr-1 mb-1">حذف</a>
+                                                                <a href="{{route('admin.brands.delete',$brand -> id)}}"
+                                                                   class="btn btn-outline-danger btn-min-width box-shadow-3 mr-1 mb-1">حذف</a>
 
 
 
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-                                                    @endforeach
-                                                @endisset
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            @endisset
 
 
                                             </tbody>
