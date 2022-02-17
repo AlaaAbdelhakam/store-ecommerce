0bf5f0a6b36da63da69b95e79ddac6adf7e26d6e
AlaaAbdelhakam <alaa.abdelhakam@hotmail.com>
2022-02-17 10:15:19 +0200

    =

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(SettingDatabaseSeeder::class);
-        
+        $this->call(SubCategoryDatabaseSeeder::class);
         $this->call(CategoryDatabaseSeeder::class);
     }
-}   
+}
--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -2,6 +2,7 @@
     <div class="main-menu-content">
 
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+
             <li class="nav-item active"><a href=""><i class="la la-mouse-pointer"></i><span class="menu-title"
                         data-i18n="nav.add_on_drag_drop.main">الرئيسية </span></a>
             </li>
@@ -24,33 +25,54 @@
 
             <li class="nav-item"><a href=""><i class="la la-group"></i>
                     <span class="menu-title" data-i18n="nav.dash.main">الاقسام الرئيسيه </span>
-                    <span class="badge badge badge-danger badge-pill float-right mr-2"></span>
+                    <span
+                        class="badge badge badge-danger badge-pill float-right mr-2">{{ \App\Models\Category::parent()->count() }}
+                    </span>
                 </a>
                 <ul class="menu-content">
-                    <li class="active"><a class="menu-item" href="" data-i18n="nav.dash.ecommerce"> عرض
-                            الكل </a>
+                    <li class="active"><a class="menu-item" href="{{ route('admin.maincategories') }}"
+                            data-i18n="nav.dash.ecommerce"> عرض الكل </a>
                     </li>
-                    <li><a class="menu-item" href="" data-i18n="nav.dash.crypto">أضافة
+                    <li><a class="menu-item" href="{{ route('admin.maincategories.create') }}"
+                            data-i18n="nav.dash.crypto">أضافة
                             قسم جديد </a>
                     </li>
                 </ul>
             </li>
 
-            </li>
-
             <li class="nav-item"><a href=""><i class="la la-group"></i>
                     <span class="menu-title" data-i18n="nav.dash.main">الاقسام الفرعية </span>
-                    <span class="badge badge badge-danger badge-pill float-right mr-2">400</span>
+                    <span
+                        class="badge badge badge-danger badge-pill float-right mr-2">{{ \App\Models\Category::child()->count() }}</span>
                 </a>
                 <ul class="menu-content">
-                    <li class="active"><a class="menu-item" href=" " data-i18n="nav.dash.ecommerce"> عرض
-                            الكل </a>
+                    <li class="active"><a class="menu-item" href="{{ route('admin.subcategories') }}"
+                            data-i18n="nav.dash.ecommerce"> عرض الكل </a>
                     </li>
-                    <li><a class="menu-item" href=" " data-i18n="nav.dash.crypto">أضافة
+                    <li><a class="menu-item" href="{{ route('admin.subcategories.create') }}"
+                            data-i18n="nav.dash.crypto">أضافة
                             قسم فرعي جديد </a>
                     </li>
                 </ul>
             </li>
+
+
+            <li class="nav-item"><a href=""><i class="la la-group"></i>
+                    <span class="menu-title" data-i18n="nav.dash.main"> الماركات التجارية </span>
+                    <span
+                        class="badge badge badge-danger badge-pill float-right mr-2">{{ \App\Models\Brand::count() }}</span>
+                </a>
+                <ul class="menu-content">
+                    <li class="active"><a class="menu-item" href="{{ route('admin.brands') }}"
+                            data-i18n="nav.dash.ecommerce"> عرض الكل </a>
+                    </li>
+                    <li><a class="menu-item" href="{{ route('admin.brands.create') }}"
+                            data-i18n="nav.dash.crypto">أضافة
+                            ماركة جديده </a>
+                    </li>
+                </ul>
+            </li>
+
 
             <li class="nav-item"><a href=""><i class="la la-male"></i>
                     <span class="menu-title" data-i18n="nav.dash.main">المتاجر </span>
@@ -95,23 +117,21 @@
             </li>
 
 
-
-
-            <li class=" nav-item"><a href=""><i class="la la-television"></i><span class="menu-title"
-                        data-i18n="nav.templates.main">{{__('admin/sidebar.settings')}}</span></a>
+            <li class=" nav-item"><a href="#"><i class="la la-television"></i><span class="menu-title"
+                        data-i18n="nav.templates.main"> {{ __('admin/sidebar.settings') }}</span></a>
                 <ul class="menu-content">
-                    <li><a class="menu-item" href="" data-i18n="nav.templates.vert.main">{{__('admin/sidebar.shipping methods')}} </a>
+                    <li><a class="menu-item" href="#" data-i18n="nav.templates.vert.main">
+                            {{ __('admin/sidebar.shipping methods') }} </a>
                         <ul class="menu-content">
                             <li><a class="menu-item" href="{{ route('edit.shippings.methods', 'free') }}"
-                                    data-i18n="nav.templates.vert.classic_menu">توصيل
-                                    مجاني </a>
+                                    data-i18n="nav.templates.vert.classic_menu">توصيل مجاني </a>
                             </li>
-                            <li><a class="menu-item" href="{{ route('edit.shippings.methods', 'inner') }}"> توصيل
+                            <li><a class="menu-item" href="{{ route('edit.shippings.methods', 'inner') }}">
+                                    توصيل
                                     داخلي </a>
                             </li>
                             <li><a class="menu-item" href="{{ route('edit.shippings.methods', 'outer') }}"
-                                    data-i18n="nav.templates.vert.compact_menu"> توصيل
-                                    خارجي </a>
+                                    data-i18n="nav.templates.vert.compact_menu"> توصيل خارجي </a>
                             </li>
                         </ul>
                     </li>
@@ -121,6 +141,7 @@
 
 
         </ul>
+
 
     </div>
 </div>
